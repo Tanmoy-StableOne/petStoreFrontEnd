f3e86f2b73caf3483da4d46949958adc70b37df6
AlphaTanmoy <dtanmoy169@gmail.com>
2025-04-03 23:25:12 +0530

    added - view

---
+++ src/app/master/admin/view-admins/view-admins.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-admins',
+  imports: [],
+  templateUrl: './view-admins.component.html',
+  styleUrl: './view-admins.component.css'
+})
+export class ViewAdminsComponent {
+
+}

8ca4ff8860108d9dcc953493f9a8fec941259f3e
AlphaTanmoy <dtanmoy169@gmail.com>
2025-04-03 23:24:44 +0530

    added - error

---
+++ src/app/common-component/un-authorize/un-authorize.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-un-authorize',
+  imports: [],
+  templateUrl: './un-authorize.component.html',
+  styleUrl: './un-authorize.component.css'
+})
+export class UnAuthorizeComponent {
+
+}

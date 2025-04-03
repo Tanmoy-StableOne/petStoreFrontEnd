348fc70a182bb370aef8bc8ed4437749a06f58c6
AlphaTanmoy <dtanmoy169@gmail.com>
2025-04-03 23:23:21 +0530

    added - read me

---
+++ src/app/seller/register-seller/register-seller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-seller',
+  imports: [],
+  templateUrl: './register-seller.component.html',
+  styleUrl: './register-seller.component.css'
+})
+export class RegisterSellerComponent {
+
+}

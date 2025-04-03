c45dbcc3f62db9b685c4da9800cfdc0d11f1cb74
AlphaTanmoy <dtanmoy169@gmail.com>
2025-04-03 23:25:23 +0530

    added - view

---
+++ src/app/master/doctor/view-doctors/view-doctors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-doctors',
+  imports: [],
+  templateUrl: './view-doctors.component.html',
+  styleUrl: './view-doctors.component.css'
+})
+export class ViewDoctorsComponent {
+
+}

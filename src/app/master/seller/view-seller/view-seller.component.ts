f9b4e8362f8a3accc5ddc52cc34035c2520e205d
AlphaTanmoy <dtanmoy169@gmail.com>
2025-04-03 23:26:06 +0530

    added - view

---
+++ src/app/master/seller/view-seller/view-seller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-seller',
+  imports: [],
+  templateUrl: './view-seller.component.html',
+  styleUrl: './view-seller.component.css'
+})
+export class ViewSellerComponent {
+
+}

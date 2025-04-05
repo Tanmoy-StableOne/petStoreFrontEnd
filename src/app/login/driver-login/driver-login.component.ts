92dde1f0b7da5a365cb991d88ae367d900f29c8b
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-05 22:13:49 +0530

    driver-login added

---
+++ src/app/login/driver-login/driver-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-login',
+  imports: [],
+  templateUrl: './driver-login.component.html',
+  styleUrl: './driver-login.component.css'
+})
+export class DriverLoginComponent {
+
+}

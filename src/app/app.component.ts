7237e1fbb3256c52809863a395f7e8db74b3df4f
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-05 13:15:06 +0530

    updated auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SideNavBarComponent } from './layout/side-nav-bar/side-nav-bar.component';
-import { VerticalNavBarComponent } from './layout/vertical-nav-bar/vertical-nav-bar.component';
 import { MenuService } from './service/menu/menu.service';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    SideNavBarComponent,
-    VerticalNavBarComponent,
-    AsyncPipe
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']

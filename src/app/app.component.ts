54e05fad605a53776ea01ae20afa9581c57cdb9d
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-05 14:33:53 +0530

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SideNavbarComponent } from './layout/side-navbar/side-navbar.component';
+import { AuthService } from './service/auth/Auth.Service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [RouterOutlet, CommonModule, NavbarComponent, SideNavbarComponent],
+  template: `
+    <!-- Top Navbar for guests -->
+    <div *ngIf="!useSideNavbar">
+      <app-navbar></app-navbar>
+    </div>
+
+    <!-- Side Navbar for logged-in users -->
+    <div *ngIf="useSideNavbar">
+      <app-side-navbar></app-side-navbar>
+    </div>
+
+    <!-- Main content -->
+    <main [class.with-side-navbar]="useSideNavbar">
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'petStoreFrontEnd';
+  useSideNavbar = false;
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.loginStatus$.subscribe((status: boolean) => {
+      this.isLoggedIn = status;
+      this.useSideNavbar = status;
+    });
+  }
 }
